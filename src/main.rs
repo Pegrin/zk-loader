539afe7305b0d2d114e6b523be2bd4f8a2c8a6fa
Khayrutdinov Marat <mail@wtiger.org>
2019-10-16 07:46:03 +0300

    Add args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env::args;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("zk-loader")
+        .version("0.0.1")
+        .author("Khayrutdinov Marat <mail@wtiger.org>")
+        .about("Downloads and uploads zookeeper znodes data")
+        .arg(Arg::with_name("download")
+                 .short("d")
+                 .long("download")
+                 .value_name("ZNODE")
+                 .help("Download data from znode")
+                 .takes_value(true), )
+        .arg(Arg::with_name("upload")
+                 .short("u")
+                 .long("upload")
+                 .value_name("ZNODE")
+                 .help("Upload data to znode")
+                 .takes_value(true), )
+        .get_matches();
 }
