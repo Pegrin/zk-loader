b51b61208ebcc4170689e94bed6e5ba649c72536
Marat Khayrutdinov <mail@wtiger.org>
2019-10-16 13:53:44 +0300

    Add arg parsing and apache license (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("zk-loader")
+        .version("0.0.1")
+        .author("Khayrutdinov Marat <mail@wtiger.org>")
+        .about("Downloads and uploads zookeeper znodes data")
+        .arg(Arg::with_name("download")
+                 .short("d")
+                 .long("download")
+                 .value_name("ZNODE")
+                 .help("Download data from znode")
+                 .takes_value(true), )
+        .arg(Arg::with_name("upload")
+                 .short("u")
+                 .long("upload")
+                 .value_name("ZNODE")
+                 .help("Upload data to znode")
+                 .takes_value(true), )
+        .get_matches();
 }
