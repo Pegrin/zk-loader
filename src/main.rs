bfabe1ea198ece03af5fc00826b7634393005bc7
Marat Khayrutdinov <mail@wtiger.org>
2019-11-04 23:55:23 +0300

    Add dump, restore functions.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
-extern crate arguments;
 extern crate clap;
+extern crate flate2;
+extern crate tar;
+
+use clap::Values;
 
 use arguments::args_parser_config;
-use clap::Values;
+
+mod zk_interaction;
+mod arguments;
 
 fn main() {
     let parser = args_parser_config();
     let args = parser.get_matches();
     if args.is_present("dump") {
-        println!("Dump data from host {hosts:?} with znodes {znodes:?} except znodes {excluded:?} to file {file:?}",
-                 hosts = args.value_of("servers").unwrap(),
-                 znodes = args.values_of("znodes").unwrap().collect::<Vec<&str>>(),
-                 excluded = args.values_of("excluded").get_or_insert(Values::default()).collect::<Vec<&str>>(),
-                 file = args.value_of("file").unwrap(),
-        )
+        let servers = args.value_of("servers").unwrap();
+        let znodes = args.values_of("znodes").unwrap().collect::<Vec<&str>>();
+        let file = args.value_of("file").unwrap();
+        let mut excluded = args.values_of("excluded").get_or_insert(Values::default()).collect::<Vec<&str>>();
+        excluded.push("/zookeeper");
+        zk_interaction::dump(servers, znodes, file, excluded);
     } else if args.is_present("restore") {
-        println!("Restore data from file {file:?} to hosts {hosts:?} with znodes {znodes:?} except znodes {excluded:?} ",
-                 hosts = args.value_of("servers").unwrap(),
-                 znodes = args.values_of("znodes").unwrap().collect::<Vec<&str>>(),
-                 excluded = args.values_of("excluded").get_or_insert(Values::default()).collect::<Vec<&str>>(),
-                 file = args.value_of("file").unwrap(),
-        )
+        let servers = args.value_of("servers").unwrap();
+        let znodes = args.values_of("znodes").unwrap().collect::<Vec<&str>>();
+        let file = args.value_of("file").unwrap();
+        let mut excluded = args.values_of("excluded").get_or_insert(Values::default()).collect::<Vec<&str>>();
+        excluded.push("/zookeeper");
+        zk_interaction::restore(servers, file, znodes, excluded);
     } else {
         panic!("Expected flag dump or restore, but achieved unexpected state.")
     }
+    println!("Success!");
 }
---
+++ src/zk_interaction.rs
@@ -0,0 +1,185 @@
+extern crate zookeeper;
+
+use std::borrow::BorrowMut;
+use std::fs::File;
+use std::io::Read;
+use std::time::Duration;
+
+use flate2::Compression;
+use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
+use tar::{Archive, Builder, Header};
+
+use self::zookeeper::{Acl, CreateMode, ZooKeeper};
+
+pub fn dump(servers: &str, znode_paths: Vec<&str>, dump_file: &str, excluded_znodes: Vec<&str>) {
+    let zk_client = ZooKeeper::connect(servers, Duration::from_secs(15), |_| {}).unwrap();
+    zk_client.exists("/", false).expect("Connection failed");
+    for znode_path in &znode_paths {
+        if zk_client.exists(*znode_path, false).unwrap().is_none() {
+            panic!("Expected znode is absent: {}", *znode_path);
+        }
+    }
+    for tree_root_znode_path in znode_paths {
+        dump_znode_tree(&zk_client, tree_root_znode_path, dump_file, &excluded_znodes);
+    }
+}
+
+pub fn restore(servers: &str, dump_file: &str, znode_paths: Vec<&str>, excluded_znodes: Vec<&str>) {
+    let zk_client = ZooKeeper::connect(servers, Duration::from_secs(15), |_| {}).unwrap();
+    zk_client.exists("/", false).expect("Connection failed");
+    let tar_gz = File::open(dump_file).expect("Can't read tar file");
+    let tar = GzDecoder::new(tar_gz);
+    let mut archive = Archive::new(tar);
+    let entries = archive.entries().expect("Can't unpack tar file");
+    for file in entries {
+        let mut file = file.unwrap();
+        let mut data: Vec<u8> = Vec::new();
+        file.read_to_end(&mut data).unwrap();
+        let path = file.path().unwrap();
+        let znode_path = tar_path_to_znode_path(path.to_str().unwrap());
+        let is_excluded = excluded_znodes.iter()
+            .any(|excluded| znode_path.starts_with(excluded));
+        if !is_excluded {
+            create_znodes_for_path(&zk_client, znode_path.as_str(), data);
+        }
+    }
+}
+
+fn create_znodes_for_path(zk_client: &ZooKeeper, path: &str, data: Vec<u8>) {
+    println!("income {:?}", path);
+    let split: Vec<&str> = path.split('/').collect();
+    println!("splited {:?}", split);
+    for i in 1..split.len() {
+        let new_znode = path_from_n_first_znodes(&split, i);
+        println!("create {}", new_znode.as_str());
+        zk_client.create(new_znode.as_str(), vec![], Acl::open_unsafe().clone(), CreateMode::Persistent);
+    }
+    let new_znode = path_from_n_first_znodes(&split, split.len() - 1);
+    zk_client.set_data(new_znode.as_str(), data, Option::None).unwrap();
+}
+
+fn path_from_n_first_znodes(split_path: &Vec<&str>, n: usize) -> String {
+    split_path.iter()
+        .skip(1)
+        .take(n)
+        .fold(String::new(), |acc, node| acc + "/" + node)
+}
+
+fn dump_znode_tree(zk_client: &ZooKeeper, tree_root_znode_path: &str, dump_file: &str, excluded_znodes: &Vec<&str>) {
+    let dump_file = File::create(dump_file).expect(format!("Can't create file '{}'", dump_file).as_str());
+    let enc = GzEncoder::new(dump_file, Compression::fast());
+    let mut tar_archive = tar::Builder::new(enc);
+    dump_znodes_recursively(zk_client, tree_root_znode_path, excluded_znodes, tar_archive.borrow_mut());
+    tar_archive.finish().unwrap();
+}
+
+fn dump_znodes_recursively(zk_client: &ZooKeeper, znode_path: &str, excluded_znodes: &Vec<&str>, tar_archive: &mut Builder<GzEncoder<File>>) {
+    if excluded_znodes.contains(&znode_path) {
+        return;
+    }
+    let (data, _) = zk_client.get_data(znode_path, false).unwrap();
+    write_znode_data_to_tar(znode_path, data, tar_archive);
+    let children = zk_client.get_children(znode_path, false).unwrap();
+    let current_path = ensure_ends_with_slash(znode_path);
+    children.iter()
+        .map(|child| current_path.clone() + child)
+        .for_each(|child| dump_znodes_recursively(zk_client, child.as_str(), excluded_znodes, tar_archive));
+}
+
+fn ensure_ends_with_slash(znode_path: &str) -> String {
+    let mut current_path = String::from(znode_path);
+    if !current_path.ends_with("/") {
+        current_path.push('/');
+    }
+    current_path
+}
+
+fn write_znode_data_to_tar(znode_path: &str, data: Vec<u8>, tar_archive: &mut Builder<GzEncoder<File>>) {
+    let mut header = Header::new_gnu();
+    header.set_size(data.len() as u64);
+    header.set_cksum();
+    let tar_path = znode_path_to_tar_path(znode_path);
+    println!("Writing: {}", tar_path);
+    tar_archive.append_data(&mut header, tar_path, data.as_slice()).unwrap();
+}
+
+fn znode_path_to_tar_path(znode_path: &str) -> String {
+    Option::from(String::from(znode_path))
+        .map(|path| path + "/____data")
+        .map(|path| path.chars()
+            .skip_while(|char| char == &'/')
+            .collect::<String>())
+        .unwrap()
+}
+
+fn tar_path_to_znode_path(tar_path: &str) -> String {
+    Option::from(String::from(tar_path))
+        .map(|path| path.replace("____data", ""))
+        .map(|path| {
+            if path.ends_with("/") {
+                path.chars()
+                    .take(path.len() - 1)
+                    .collect()
+            } else {
+                path
+            }
+        })
+        .map(|path| if !path.starts_with("/") { String::from("/") + path.as_str() } else { path })
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use zk_interaction::{dump, restore, tar_path_to_znode_path, znode_path_to_tar_path};
+
+    use super::zookeeper::{Acl, CreateMode, ZooKeeper};
+
+    fn zk_client() -> ZooKeeper {
+        ZooKeeper::connect("localhost:2181", Duration::from_secs(15), |_| {}).unwrap()
+    }
+
+    #[test]
+    pub fn test_dump_restore() {
+        let zk = zk_client();
+        let dump_file = "test-dump-file.tar.gz";
+        let root_znode = ("/test_ase2134234", b"123data!".to_vec());
+        let excluded_znode = ("/test_ase2134234/2", b"123data!+2".to_vec());
+        let child_znode = ("/test_ase2134234/1", b"123data!+1".to_vec());
+
+        zk.create(root_znode.0, root_znode.1.clone(), Acl::open_unsafe().clone(), CreateMode::Persistent);
+        zk.create(child_znode.0, child_znode.1.clone(), Acl::open_unsafe().clone(), CreateMode::Persistent);
+        zk.create(excluded_znode.0, excluded_znode.1.clone(), Acl::open_unsafe().clone(), CreateMode::Persistent);
+
+        dump("localhost:2181", vec![root_znode.0], dump_file, vec![excluded_znode.0]);
+        zk.delete(child_znode.0, None);
+        zk.delete(excluded_znode.0, None);
+        zk.delete(root_znode.0, None);
+        restore("localhost:2181", dump_file, vec![root_znode.0], vec![excluded_znode.0]);
+
+        assert_eq!(zk.get_data(child_znode.0, false).unwrap().0, child_znode.1);
+        assert_eq!(zk.get_data(root_znode.0, false).unwrap().0, root_znode.1);
+        assert!(zk.exists(excluded_znode.0, false).unwrap().is_none())
+    }
+
+    #[test]
+    pub fn tar_path_to_znode_path_test() {
+        let zk_path = tar_path_to_znode_path("____data");
+        assert_eq!(zk_path, "/");
+
+        let zk_path = tar_path_to_znode_path("banana/____data");
+        assert_eq!(zk_path, "/banana");
+    }
+
+    #[test]
+    pub fn znode_path_to_tar_path_test() {
+        let tar_path = znode_path_to_tar_path("/");
+        assert_eq!(tar_path, "____data");
+
+        let tar_path = znode_path_to_tar_path("/banana");
+        assert_eq!(tar_path, "banana/____data");
+    }
+}
+
